8f89e75af386e5aec767407803546f1ad2f10928
Blank Spruce <>
2019-08-03 15:47:30 +0200

    Cleaned up ui.el

--- ui.el
+++ ui.el
@@ -15,12 +15,10 @@
   ;; Others
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
+  (menu-bar-mode 1)
   )
 
-(if (display-graphic-p)
-    (progn
-      (menu-bar-mode 1)
-      )
+(when (not (display-graphic-p))
   (menu-bar-mode -1)
   )
 
